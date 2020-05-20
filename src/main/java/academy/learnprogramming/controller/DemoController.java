92ebdfdd1f051718a6db9baaded83707b863b50c
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-05-19 20:51:19 -0500

    9. Model and Model Attributes

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,9 +1,13 @@
 package academy.learnprogramming.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
     // We´re always going to annotate our web controllers with the controller annotation
@@ -19,7 +23,15 @@ public class DemoController {
     // prefix + name + suffix
     // /WEB-INF/view/welcome.jsp
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("user", "Tim");
+        log.info("model= {}", model);
         return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "welcome to this Demo application";
     }
 }
