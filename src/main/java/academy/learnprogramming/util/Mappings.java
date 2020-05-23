5f6bd19b097d883f59da0adf954ea2178dddd6ee
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-05-22 23:47:49 -0500

    15. Creating the Todo Item Controller

---
+++ src/main/java/academy/learnprogramming/util/Mappings.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming.util;
+
+public final class Mappings {
+    // == constants ==
+    public static final String ITEMS = "items";
+
+    // == constructor ==
+    private Mappings() {}
+}
---
+++ src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+    // == constants ==
+    public static final String ITEMS_LIST = "items";
+
+    // == constructor ==
+    private ViewNames() {}
+}
